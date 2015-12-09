6527b0d1b831842a350e0651cd92efe7447f4f3d
René Montes de Oca <reneamontes@gmail.com>
2015-12-08 18:46:09 -0600

    Fixes

--- app/orm/Config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once "rb.php";
-$config = json_decode(file_get_contents(__DIR__. "/../../.config/database.json"));
-R::setup($config->driver.':host='.$config->host.';dbname='.$config->dbname,$config->user,$config->pass);
-R::useWriterCache(true);
